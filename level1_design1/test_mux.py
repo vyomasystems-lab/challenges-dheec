c83f8b56177635d0cf0208739229f0392d9f5f4c
dheec <dheerajchumble@gmail.com>
2022-07-24 11:37:24 +0000

    update mux test

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -9,12 +9,12 @@ async def test_mux(dut):
     """Test for mux2"""
 
     cocotb.log.info('##### CTB: Develop your test here ########')
-    sel1 = 5'b00000;
-    inp00 = 0;
-    inp11 = 1;
-    dut.sel.value = 00000;
-    dut.inp0.value = inp00;
-    dut.inp0.value = inp11;
+    #sel1 = 5'b00000
+    #inp00 = 0
+    #inp11 = 1
+    dut.sel.value = 00000
+    dut.inp0.value = 0
+    dut.inp0.value = 1
     #dut.inp0.value = 02;
     #dut.inp0.value = 03;
     #dut.inp0.value = 00;
