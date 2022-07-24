f16cbf6d5cd67ebb16c6602df1908e2526077d0c
dheec <dheerajchumble@gmail.com>
2022-07-24 10:33:12 +0000

    updated mux test

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -2,9 +2,28 @@
 
 import cocotb
 from cocotb.triggers import Timer
+import random
 
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
 
     cocotb.log.info('##### CTB: Develop your test here ########')
+    
+    dut.sel.value = 0;
+    dut.inp0.value = 00;
+    #dut.inp0.value = 01;
+    #dut.inp0.value = 02;
+    #dut.inp0.value = 03;
+    #dut.inp0.value = 00;
+
+    await Timer(2, units='ns')
+
+    assert dut.out.value == 0, f"Mux result is incorrect: {dut.X.value} != 0"
+
+    #for i in range(5):
+    #    sel1  = random.randint(0, 31)
+
+    #    dut.sel.value = sel1
+    #    await Timer(2, units='ns')
+    
