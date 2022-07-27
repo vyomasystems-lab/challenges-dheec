5afc19b69e56c60754fe551ad099c7f17bf3dfb9
dheec <dheerajchumble@gmail.com>
2022-07-27 18:35:11 +0000

    updated mux test

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -13,7 +13,7 @@ async def test_mux(dut):
     
     
     #for i in range(4):
-    dut.sel.value = 30
+    dut.sel.value = 3
     await Timer(2, units='ns')
     if dut.sel.value == 0:
          dut.inp0.value = 0
@@ -81,7 +81,7 @@ async def test_mux(dut):
     
     await Timer(2, units='ns')
 
-    assert dut.out.value == dut.inp30.value, f"Mux result is incorrect: {dut.out.value} != {dut.inp30.value}"
+    assert dut.out.value == dut.inp3.value, f"Mux result is incorrect: {dut.out.value} != {dut.inp3.value}"
     print("                      inp30 : %d, out : %d, sel : %d" % (dut.inp30.value,dut.out.value,dut.sel.value))
     
     
