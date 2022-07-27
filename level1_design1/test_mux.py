8ce8cdaaa3fc11e4a85cc48fa1511af649d55435
dheec <dheerajchumble@gmail.com>
2022-07-27 17:49:49 +0000

    updated mux test

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -11,72 +11,73 @@ async def test_mux(dut):
     cocotb.log.info('##### CTB: Develop your test here ########')
         
     
-    tmp = 1
+    
     #for i in range(4):
     dut.sel.value = 30
     await Timer(2, units='ns')
     if dut.sel.value == 0:
-         dut.inp0.value = dut.tmp.value 
+         dut.inp0.value = 0
     elif dut.sel.value == 1:
-          dut.inp1.value = dut.tmp.value 
+          dut.inp1.value = 1
     elif dut.sel.value == 2:
-          dut.inp2.value = dut.tmp.value 
+          dut.inp2.value = 2
     elif dut.sel.value == 3:
-          dut.inp3.value = dut.tmp.value 
+          dut.inp3.value = 3 
     elif dut.sel.value == 4:
-          dut.inp4.value = dut.tmp.value 
+          dut.inp4.value = 0
     elif dut.sel.value == 5:
-          dut.inp5.value = dut.tmp.value 
+          dut.inp5.value = 1
     elif dut.sel.value == 6:
-          dut.inp6.value = dut.tmp.value 
+          dut.inp6.value = 2
     elif dut.sel.value == 7:
-          dut.inp7.value = dut.tmp.value 
+          dut.inp7.value = 3
     elif dut.sel.value == 8:
-          dut.inp8.value = dut.tmp.value 
+          dut.inp8.value = 0
     elif dut.sel.value == 9:
-          dut.inp9.value = dut.tmp.value 
+          dut.inp9.value = 1
     elif dut.sel.value == 10:
-          dut.inp10.value = dut.tmp.value 
+          dut.inp10.value = 2
     elif dut.sel.value == 11:
-          dut.inp11.value = dut.tmp.value 
+          dut.inp11.value = 3
     elif dut.sel.value == 12:
-          dut.inp12.value = dut.tmp.value 
+          dut.inp12.value = 0
     elif dut.sel.value == 13:
-          dut.inp13.value = dut.tmp.value 
+          dut.inp13.value = 1
     elif dut.sel.value == 14:
-          dut.inp14.value = dut.tmp.value 
+          dut.inp14.value = 2
     elif dut.sel.value == 15:
-          dut.inp15.value = dut.tmp.value 
+          dut.inp15.value = 3
     elif dut.sel.value == 16:
-          dut.inp17.value = dut.tmp.value 
+          dut.inp17.value = 0
     elif dut.sel.value == 18:
-          dut.inp18.value = dut.tmp.value 
+          dut.inp18.value = 1
     elif dut.sel.value == 19:
-          dut.inp19.value = dut.tmp.value 
+          dut.inp19.value = 2
     elif dut.sel.value == 20:
-          dut.inp20.value = dut.tmp.value 
+          dut.inp20.value = 3
     elif dut.sel.value == 21:
-         dut.inp21.value = dut.tmp.value 
+         dut.inp21.value = 0
     elif dut.sel.value == 22:
-          dut.inp22.value = dut.tmp.value 
+          dut.inp22.value = 1
     elif dut.sel.value == 23:
-          dut.inp13.value = dut.tmp.value 
+          dut.inp13.value = 2
     elif dut.sel.value == 24:
-          dut.inp24.value = dut.tmp.value 
+          dut.inp24.value = 3
     elif dut.sel.value == 25:
-         dut.inp25.value = dut.tmp.value 
+         dut.inp25.value = 0
     elif dut.sel.value == 26:
-          dut.inp26.value = dut.tmp.value 
+          dut.inp26.value = 1
     elif dut.sel.value == 27:
-          dut.inp27.value = dut.tmp.value 
+          dut.inp27.value = 2
     elif dut.sel.value == 28:
-          dut.inp28.value = dut.tmp.value 
+          dut.inp28.value = 3
     elif dut.sel.value == 29:
-          dut.inp29.value = dut.tmp.value 
+          dut.inp29.value = 0
     elif dut.sel.value == 30:
-          dut.inp30.value = dut.tmp.value 
+          dut.inp30.value = 1
+          dut.tmp.value = dut.inp30.value
     
-    dut.out.value = dut.tmp.value  
+    
     
     
     await Timer(2, units='ns')
