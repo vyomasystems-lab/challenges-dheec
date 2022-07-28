2f8603889e0be2b59adbc3df15ea31ff652d55bd
dheec <dheerajchumble@gmail.com>
2022-07-28 17:30:14 +0000

    updated mux test

--- level1_design1/Readme.md
+++ level1_design1/Readme.md
@@ -18,3 +18,14 @@ Design Bug
 Based on the above test input and analysing the design, we see the following
 For 5 bit 'select' lines there should be 32 input lines.
 
+Test Scenario 
+Test Inputs: sel = 1
+Expected Output: out = 0
+Observed Output in the DUT dut.out= 'Z'
+Error:ValueError : Unresolvable bit in binary string: 'z'
+Output mismatches for the above inputs proving that there is a design bug
+
+Design Bug
+Based on the above test input and analysing the design, we see the following
+Output should be'Zero' When only 'Select signal ' is there
+
--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -13,47 +13,47 @@ async def test_mux(dut):
     
     
     #for i in range(4):
-    dut.sel.value = 1
+    dut.sel.value = x
     await Timer(2, units='ns')
-    #dut.inp0.value = 0    
-    #dut.inp1.value = 1
-    #dut.inp2.value = 2    
-    #dut.inp3.value = 3     
-    #dut.inp4.value = 0    
-    #dut.inp5.value = 1    
-    #dut.inp6.value = 2    
-    #dut.inp7.value = 3   
-    #dut.inp8.value = 0    
-    #dut.inp9.value = 1    
-    #dut.inp10.value = 2    
-    #dut.inp11.value = 3    
-    #dut.inp12.value = 0    
-    #dut.inp13.value = 1    
-    #dut.inp14.value = 2    
-    #dut.inp15.value = 3    
-    #dut.inp17.value = 0    
-    #dut.inp18.value = 1    
-    #dut.inp19.value = 2    
-    #dut.inp20.value = 3    
-    #dut.inp21.value = 0    
-    #dut.inp22.value = 1    
-    #dut.inp13.value = 2    
-    #dut.inp24.value = 3    
-    #dut.inp25.value = 0    
-    #dut.inp26.value = 1    
-    #dut.inp27.value = 2    
-    #dut.inp28.value = 3    
-    #dut.inp29.value = 0    
-    #dut.inp30.value = 1
-    #dut.inp31.value = 2
+    dut.inp0.value = 0    
+    dut.inp1.value = 1
+    dut.inp2.value = 2    
+    dut.inp3.value = 3     
+    dut.inp4.value = 0    
+    dut.inp5.value = 1    
+    dut.inp6.value = 2    
+    dut.inp7.value = 3   
+    dut.inp8.value = 0    
+    dut.inp9.value = 1    
+    dut.inp10.value = 2    
+    dut.inp11.value = 3    
+    dut.inp12.value = 0    
+    dut.inp13.value = 1    
+    dut.inp14.value = 2    
+    dut.inp15.value = 3    
+    dut.inp17.value = 0    
+    dut.inp18.value = 1    
+    dut.inp19.value = 2    
+    dut.inp20.value = 3    
+    dut.inp21.value = 0    
+    dut.inp22.value = 1    
+    dut.inp13.value = 2    
+    dut.inp24.value = 3    
+    dut.inp25.value = 0    
+    dut.inp26.value = 1    
+    dut.inp27.value = 2    
+    dut.inp28.value = 3    
+    dut.inp29.value = 0    
+    dut.inp30.value = 1
+    dut.inp31.value = 2
           
     
     
     
     await Timer(2, units='ns')
 
-    #assert dut.out.value == dut.inp32.value, f"Mux result is incorrect: {dut.out.value} != {dut.inp32.value}"
-    print("                       out : %d, sel : %d" % (dut.out.value,dut.sel.value))
+    assert dut.out.value == dut.inp29.value, f"Mux result is incorrect: {dut.out.value} != {dut.inp29.value}"
+    print("                 inp29  : %d, out : %d, sel : %d" % (dut.out.value,dut.sel.value))
     
     
     
