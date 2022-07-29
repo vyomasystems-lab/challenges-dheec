0476259b6d65e13defd7308feb5e585a924dad61
dheec <dheerajchumble@gmail.com>
2022-07-29 18:35:50 +0000

    updated mux test

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -25,7 +25,7 @@ async def test_seq_bug1(dut):
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
 
-    for i in range (10):
+    for i in range (50):
         inp_bit = random.randint(0, 1)
         print("inp_bit = %d" % inp_bit)
         print("seq_seen = %d"%dut.seq_seen.value)
