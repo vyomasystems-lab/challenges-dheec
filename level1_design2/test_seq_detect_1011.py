113684b830b91facbce559c5753bba21f00dd450
dheec <dheerajchumble@gmail.com>
2022-07-29 13:33:15 +0000

    updated fsm test

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -25,7 +25,7 @@ async def test_seq_bug1(dut):
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
 
-    for i in range (6):
+    for i in range (10):
         inp_bit = random.randint(0, 1)
         print("inp_bit = %d" % inp_bit)
-    print("next_state = %s" % next_state)
+    
